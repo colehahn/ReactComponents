814d48e7a9ce4350baec5a9c7183ed0f19c9da23
colehahn <colehahn2000@gmail.com>
2024-03-10 10:36:03 -0700

    removed styled-components
    i heard it can increase load tme since it means all js must be loaded before styles appear

--- src/components/CopyCode.tsx
+++ src/components/CopyCode.tsx
@@ -1,84 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 import { BiCopy } from "react-icons/bi";
-
-const borderRadius = "1em";
-
-const Wrapper = styled.div`
-  background: var(--transparent);
-  font-family: monospace;
-  //max-width: fit-content;
-  width: 100%;
-  position: relative;
-  border: 2px solid;
-  border-radius: ${borderRadius};
-  min-height: 60px;
-
-  .copied {
-    animation: 0.7s flash;
-  }
-  @keyframes flash {
-    from {
-      background-color: auto;
-    }
-
-    10% {
-      background-color: rgba(255, 255, 255, 0.7);
-    }
-
-    to {
-      background-color: auto;
-    }
-  }
-`;
-
-const TitleWrapper = styled.div`
-  background: var(--transparent);
-  padding-left: 2em;
-  border: 1px solid transparent; //hmmm.. this definitely isn't right
-  border-radius: ${borderRadius};
-`;
-
-const ContentWrapper = styled.div`
-  padding: 1em;
-  padding-right: 3em;
-  overflow-x: auto;
-`;
-
-const ScrollbarHider = styled.div`
-  overflow: hidden;
-  border-radius: ${borderRadius};
-`;
-
-const AtRight = styled.div`
-  position: absolute;
-  top: 0.5em;
-  right: 0.5em;
-`;
-
-const IconWrapper = styled.div`
-  font-size: 2.5em;
-  &:hover {
-    cursor: pointer;
-  }
-  border: 2px solid;
-  border-radius: 0.2em;
-  padding: 0.1em;
-  position: absolute;
-  top: 0;
-  right: 0;
-`;
-
-const StyledIcon = styled(BiCopy)`
-  display: block;
-`;
-
-const CopiedLabel = styled.label`
-  border: 1px solid;
-  background-color: var(--background);
-  position: relative;
-  top: 1em;
-`;
+import "../styles/CopyCode.css";
 
 export const CopyCode: React.FC<{
   content: any;
@@ -103,22 +25,24 @@ export const CopyCode: React.FC<{
   };
 
   return (
-    <Wrapper>
+    <div className="copycode">
       {props.title && (
-        <TitleWrapper>
+        <div className="title">
           <h3>{props.title}:</h3>
-        </TitleWrapper>
+        </div>
       )}
-      <AtRight>
-        <IconWrapper onClick={(e: any) => CopyToClipboard()}>
-          <StyledIcon />
-        </IconWrapper>
-        {copied && <CopiedLabel>copied to clipboard!</CopiedLabel>}
-      </AtRight>
-      <ScrollbarHider>
-        <ContentWrapper ref={ref}>{props.content}</ContentWrapper>
-      </ScrollbarHider>
-    </Wrapper>
+      <div className="at-right">
+        <div className="icon-wrapper" onClick={(e: any) => CopyToClipboard()}>
+          <BiCopy className="styled-icon" />
+        </div>
+        {copied && <div className="copied-label">copied to clipboard!</div>}
+      </div>
+      <div className="scrollbar-hi der">
+        <div className="content-wrapper" ref={ref}>
+          {props.content}
+        </div>
+      </div>
+    </div>
   );
 };
 
